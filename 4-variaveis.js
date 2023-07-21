2f067afcc56ec9bab1ba194716d92105b67dbee0
RodYam <rodrigo.yoshio.yamamoto@gmail.com>
2023-07-21 11:41:15 -0300

    Create 4-variaveis.js

---
+++ 4-variaveis.js
@@ -0,0 +1,44 @@
+//var
+
+/*
+var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento;
+var area;
+
+console.log(area);
+*/
+
+//let
+/*
+let forma = "Triangulo"
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if(forma === "Retangulo") {
+    area = altura * comprimento
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
+*/
+
+//Const
+
+const forma = "Triangulo";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if(forma === "Retangulo") {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
+
+
