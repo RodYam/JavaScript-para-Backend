63f5ac1964e273905106463351de7ee833f94b40
RodYam <rodrigo.yoshio.yamamoto@gmail.com>
2023-07-25 10:17:11 -0300

    Update 6-conversaoDeTipo.js

--- 6-conversaoDeTipo.js
+++ 6-conversaoDeTipo.js
@@ -19,3 +19,42 @@ console.log(numero + numeroString); // concatenação. Transformou a const numer
 
 console.log(numero + Number(numeroString));
 console.log(numeroComLetra);
+
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone));
+
+// Outra opçao para transformar um valor em String:
+console.log("O telefone é " + telefone.toString());
+
+// Podemos usar o operador de soma + para fazer a conversão de textos para números, colocando-os antes das variáveis:
+
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura); // teremos a conversão de String para números realizado usando o + antes das variáveis
+
+let meuNome = "leonardo";
+console.log(Number(meuNome)); // como a variável meuNome não contém apenas números ele retorna o erro NaN (Not a Number, não é número);
+console.log( + meuNome); // a conversão também retornará NaN
+
+let usuarioConectado = false;
+console.log(Number(usuarioConectado)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+usuarioConectado = true;
+console.log(Number(usuarioConectado)); // agora teremos a conversão de true (verdadeiro) para o número 1.
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
+// var respostaDeTudo será impresso na tela o ultimo valor da qual foi decladaro, neste caso, dentro do segundo bloco, com valor 3.14.
+
+// let idade será impresso conforme sua declaração em cada bloco.
+
+// const pi será impresso o valor atribuido em cada bloco.
