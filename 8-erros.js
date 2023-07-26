f21d74d324a407e9ab189392d19b0d3e9d5b6bd1
RodYam <rodrigo.yoshio.yamamoto@gmail.com>
2023-07-26 10:45:55 -0300

    Create 8-erros.js
    Simulando e interpretando os erros.

---
+++ 8-erros.js
@@ -0,0 +1,18 @@
+/*
+// Criando uma const sem um valor (inicializador):
+
+const numero; // Const não pode ser cirado sem valor (inicializador).
+
+*/
+
+/*
+// Nao fechando o parenteses ao final da declaração:
+
+const numero = 1;
+console.log(numero // missing ) after argument list.
+*/
+
+// Chamando uma variavel ainda não declarada:
+
+const numero = 1;
+console.log(minhaVar);
